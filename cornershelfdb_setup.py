a6b64d1fce850e370880180bc434066ec45092b9
llbarnet <lauren_barnet@outlook.com>
2018-10-09 10:58:22 -0700

    fixed to meet pep8 style requirments

--- cornershelfdb_setup.py
+++ cornershelfdb_setup.py
@@ -8,7 +8,8 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base) :
+
+class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
@@ -16,44 +17,44 @@ class User(Base) :
     cookbookName = relationship("Cookbook", backref='cookbook.name')
 
 
-class Cookbook(Base) :
+class Cookbook(Base):
     __tablename__ = 'cookbook'
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
     userID = Column(Integer, ForeignKey('user.id'))
     user = relationship("User", foreign_keys=[userID])
 
+    @property
+    def serialize(self):
+        """return object data in serializable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'userID': self.userID,
+             }
+
+
+class Recipes(Base):
+    __tablename__ = 'recipes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    ingredients = Column(String(300), nullable=False)
+    directions = Column(String(800), nullable=False)
+    type = Column(String(50))
+    cookbookID = Column(Integer, ForeignKey('cookbook.id'))
+    cookbook = relationship("Cookbook", foreign_keys=[cookbookID])
+    userID = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", foreign_keys=[userID])
 
     @property
     def serialize(self):
         """return object data in serializable format"""
         return {
-            'id' : self.id,
-            'name' : self.name,
-            'userID'  : self.userID,
-             }
-
-class Recipes(Base) :
-     __tablename__ = 'recipes'
-     id = Column(Integer, primary_key=True)
-     name = Column(String(80), nullable = False)
-     ingredients = Column(String(300), nullable = False)
-     directions = Column(String(800), nullable = False)
-     type = Column(String(50))
-     cookbookID = Column(Integer, ForeignKey('cookbook.id'))
-     cookbook = relationship("Cookbook", foreign_keys=[cookbookID])
-     userID = Column(Integer, ForeignKey('user.id'))
-     user = relationship("User", foreign_keys=[userID])
-
-     @property
-     def serialize(self):
-         """return object data in serializable format"""
-         return {
-             'id' : self.id,
-             'name' : self.name,
-             'ingredients' : self.ingredients,
-             'directions' : self.directions,
-             'type' : self.type,
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients,
+            'directions': self.directions,
+            'type': self.type,
              }
 
 engine = create_engine('sqlite:///cornershelf.db')
